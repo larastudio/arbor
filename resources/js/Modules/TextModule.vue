bbc5b04311b02b54094a1ded43ea218b2d01aa65
Jasper Frumau <jasper@imagewize.com>
2024-10-11 10:21:32 +0700

    include border in text module box size

--- resources/js/Modules/TextModule.vue
+++ resources/js/Modules/TextModule.vue
@@ -11,16 +11,18 @@
   </script>
   
   <style scoped>
-  .text-module {
-    font-size: 16px;
-    color: #333;
-    /* Ensure text fills the entire module area */
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-  }
+
+.text-module {
+  font-size: 16px;
+  color: #333;
+  /* Ensure text fills the entire module area */
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  box-sizing: border-box; /* Make sure padding/border do not exceed width/height */
+}
   </style>
   
