0352a43b6b8fa1ccfe62f9e9a204c7a86e7542c6
Jasper Frumau <jasper@imagewize.com>
2024-10-21 11:17:19 +0700

    only run https via valet when app_env is local

--- vite.config.js
+++ vite.config.js
@@ -4,27 +4,31 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import fs from 'fs';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ mode }) => {
+    const isLocal = process.env.APP_ENV === 'local';  // Check if environment is local for Valet SSL
+
+    return {
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
-            },
-        }),
-    ],
-    server: {
-        host: 'saas.test',
-        port: 5173,
-        https: {
-            key: fs.readFileSync(path.resolve(process.env.HOME, '.config/Valet/Certificates/saas.test.key')),
-            cert: fs.readFileSync(path.resolve(process.env.HOME, '.config/Valet/Certificates/saas.test.crt')),
+            }),
+        ],
+        server: {
+            host: 'arbor.test',
+            port: 5173,
+            https: isLocal ? {
+                key: fs.readFileSync(path.resolve(process.env.HOME, '.config/Valet/Certificates/arbor.test.key')),
+                cert: fs.readFileSync(path.resolve(process.env.HOME, '.config/Valet/Certificates/saas.arbor.crt')),
+            } : false,  // Disable HTTPS if not in local environment
         },
-    },
+    };
 });
