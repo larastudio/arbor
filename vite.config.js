4c14bd74dcfa05b3b9c19e5aee2d995d8b2662e6
Jasper Frumau <jasper@imagewize.com>
2024-10-27 17:46:34 +0700

    improved vite server checks

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,56 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import fs from 'fs';
 
 export default defineConfig(({ mode }) => {
-    const isLocal = process.env.APP_ENV === 'local';  // Check if environment is local for Valet SSL
+    const env = loadEnv(mode, process.cwd(), '');
+
+    if (mode === 'production') {
+        // No need to define `server` for production; skip HMR and host configuration
+        return {
+            plugins: [
+                laravel({
+                    input: 'resources/js/app.js',
+                    refresh: true,
+                }),
+                vue({
+                    template: {
+                        transformAssetUrls: {
+                            base: null,
+                            includeAbsolute: false,
+                        },
+                    },
+                }),
+            ],
+        };
+    }
+
+    // Development server configuration
+    const isValet = env.APP_ENV === 'valet';
+    const serverConfig = isValet ? {
+        host: 'arbor.test',
+        port: 5173,
+        https: {
+            key: fs.readFileSync(path.resolve(env.HOME, '.config/valet/Certificates/arbor.test.key')),
+            cert: fs.readFileSync(path.resolve(env.HOME, '.config/valet/Certificates/arbor.test.crt')),
+        },
+        hmr: {
+            protocol: 'wss',
+            host: 'arbor.test',
+            port: 5173,
+        },
+    } : {
+        host: 'arbor.test',
+        port: 5173,
+        https: env.APP_ENV !== 'lima', // HTTPS for non-Lima environments
+        hmr: {
+            protocol: env.APP_ENV === 'lima' ? 'ws' : 'wss',
+            host: 'arbor.test',
+            port: 5173,
+        },
+    };
 
     return {
         plugins: [
@@ -22,13 +67,6 @@ export default defineConfig(({ mode }) => {
                 },
             }),
         ],
-        server: {
-            host: 'arbor.test',
-            port: 5173,
-            https: isLocal ? {
-                key: fs.readFileSync(path.resolve(process.env.HOME, '.config/Valet/Certificates/arbor.test.key')),
-                cert: fs.readFileSync(path.resolve(process.env.HOME, '.config/Valet/Certificates/saas.arbor.crt')),
-            } : false,  // Disable HTTPS if not in local environment
-        },
+        server: serverConfig,
     };
 });
